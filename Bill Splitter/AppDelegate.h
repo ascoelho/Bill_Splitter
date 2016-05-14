9509ea189373b6391dbb67118f8bbf20839fcdd0
ascoelho <anthonystefancoelho@gmail.com>
2016-05-14 15:38:01 -0400

    first commit

---
+++ Bill Splitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bill Splitter
+//
+//  Created by Anthony Coelho on 2016-05-14.
+//  Copyright © 2016 Anthony Coelho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
